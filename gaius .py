6fcb64dd06aa57d6483a5256693b7f9f0e366150
aswin049 <aswindev8289@gmail.com>
2025-03-07 11:39:33 +0530

    Delete gaius .py

--- gaius .py
+++
@@ -1,8 +0,0 @@
-sum = 0
-n = int (input("Enter the limit :"))
-for a in range (n):
-    n1 = int (input("Enter a number : "))
-    sum=sum+n1
-    print(sum)
-avg = sum/n
-print( "The average is :",avg)
