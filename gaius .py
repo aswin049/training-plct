c007f96cf31cdd9d504b183ce68ba8aa594fb223
aswin049 <aswindev8289@gmail.com>
2025-03-07 11:34:17 +0530

    Add files via upload

---
+++ gaius .py
@@ -0,0 +1,8 @@
+sum = 0
+n = int (input("Enter the limit :"))
+for a in range (n):
+    n1 = int (input("Enter a number : "))
+    sum=sum+n1
+    print(sum)
+avg = sum/n
+print( "The average is :",avg)
