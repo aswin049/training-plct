421f8d8699e4645d6047a41473c1f940c9816f9d
aswin049 <aswindev8289@gmail.com>
2025-03-08 12:08:49 +0530

    Create simplesort.py

---
+++ simplesort.py
@@ -0,0 +1,6 @@
+inputList=[1,12,32,3,45,2]
+print("List before sort:",inputList)
+inputList.sort()
+print("acending order",inputList)
+inputList.sort(reverse=True)
+print("decending order",inputList)
