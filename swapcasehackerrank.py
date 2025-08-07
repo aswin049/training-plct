a86d070b0aea8ae5c6e5162f4d5017a7d7989ad0
aswin049 <aswindev8289@gmail.com>
2025-08-07 11:25:02 +0530

    Update swapcasehackerrank.py

--- swapcasehackerrank.py
+++ swapcasehackerrank.py
@@ -1,14 +1,15 @@
 def swap_case(s):
- result=''
-for i in s:       
- if i.isupper():           
- result+=i.lower()
-elif i.islower():            
-result+=i.upper()
-else:   
- result+=i
-return result
+    result = ''
+    for i in s:       
+        if i.isupper():           
+            result += i.lower()
+        elif i.islower():            
+            result += i.upper()
+        else:   
+            result += i
+    return result
 if __name__ == '__main__':
- s = input()
- result = swap_case(s)
-print(result)
+    s = input()
+    result = swap_case(s)
+    print(result)
+
